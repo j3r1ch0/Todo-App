9d14e760bb3cdfa981d2820997302bb042c98ae4
Mike <mmyers33@owlmail.harford.edu>
2021-11-14 14:46:47 -0500

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Michael Myers on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
