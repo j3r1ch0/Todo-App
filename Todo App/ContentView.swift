8b13dcf75d8fce7708e3e0f958ea8c27e3ba8c9e
Mike <mmyers33@owlmail.harford.edu>
2021-11-15 18:58:46 -0500

    ToDo App

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    
+    @State private var newTodo = ""
+    @State private var allTodos: [TodoItem] = []
 
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Add ToDo...", text: $newTodo).textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action: {
+                        guard !self.newTodo.isEmpty else { return }
+                        self.allTodos.append(TodoItem(todo: self.newTodo))
+                        self.newTodo = ""
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding(.leading, 5)
+                }.padding()
+                
+                List {
+                    ForEach(allTodos) { TodoItem in
+                        Text(TodoItem.todo)
+                    }
+                
+            }
+            .navigationBarTitle("Todos")
+        }
+    }
+    }
+    
+    private func saveTodos() {
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(self.allTodos), forKey: "todosKey")
+      }
+    private func loadTodos() {
+       if let todosData = UserDefaults.standard.value(forKey: "todosKey") as? Data {
+         if let todosList = try? PropertyListDecoder().decode(Array<TodoItem>.self, from: todosData) {
+           self.allTodos = todosList
+         }
+       }
+     }
+    private func deleteTodo(at offsets: IndexSet) {
+        self.allTodos.remove(atOffsets: offsets)
+        saveTodos()
+      }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct TodoItem: Codable, Identifiable {
+    var id = UUID()
+    let todo: String
+}
+
